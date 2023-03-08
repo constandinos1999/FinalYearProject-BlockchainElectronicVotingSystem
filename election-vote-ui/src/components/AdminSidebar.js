751a48c4d495c40853cf104d8e3a66bfa4e993d3
constandinos1999 <constantinekotsis@gmail.com>
2023-03-08 23:12:11 +0900

    add routes of admin page

--- election-vote-ui/src/components/AdminSidebar.js
+++ election-vote-ui/src/components/AdminSidebar.js
@@ -4,17 +4,17 @@ import { HiOutlineGlobe, HiUserGroup, HiViewGrid } from "react-icons/hi";
 const AdminSidebar = () => {
     return (
         <div className="side-bar w-24 fixed h-full flex flex-col gap-5 bg-white p-5">
-            <Link href="/" className="flex flex-col justify-center items-center">
+            <Link href="/admin/elections" className="flex flex-col justify-center items-center">
                 <HiViewGrid size={48}/>
                 <span className="font-bold mt-1">Elections</span>
             </Link>
 
-            <Link href="/" className="flex flex-col justify-center items-center">
+            <Link href="/admin/candidates" className="flex flex-col justify-center items-center">
                 <HiUserGroup size={48}/>
                 <span className="font-bold mt-1">Candidates</span>
             </Link>
 
-            <Link href="/" className="flex flex-col justify-center items-center">
+            <Link href="/admin/voters" className="flex flex-col justify-center items-center">
                 <HiOutlineGlobe size={48}/>
                 <span className="font-bold mt-1">Voters</span>
             </Link>
